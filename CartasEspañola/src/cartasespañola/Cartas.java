2837fa5d6294a17f567396c4687cc0da3656e99a
adamulp <adamgoestoargentina@gmail.com>
2024-03-21 19:33:20 -0300

    Agrega metodos stub

---
+++ CartasEspañola/src/cartasespañola/Cartas.java
@@ -0,0 +1,75 @@
+
+/**
+ * https://github.com/adamulp/sortingAlgos/
+ *
+ * @authors:
+ *           Alexis Allendez        https://github.com/AlexisAllendez
+ *           Enzo Casalegno         https://github.com/EnzoCasalegno
+ *           Nicolás Hollmann       https://github.com/HollmannCod3
+ *           Adam Rigg              https://github.com/adamulp/
+ */
+package cartasespañola;
+
+/**
+ *
+ * @author adam
+ */
+public class Cartas {
+    
+/*
+ • barajar(): cambia de posición todas las cartas aleatoriamente.
+
+• siguienteCarta(): devuelve la siguiente carta que está en la baraja, 
+cuando no haya más o se haya llegado al final, se indica al usuario que 
+no hay más cartas.
+
+• cartasDisponibles(): indica el número de cartas que aún se puede repartir.
+
+• darCartas(): dado un número de cartas que nos pidan, le devolveremos 
+ese número de cartas. En caso de que haya menos cartas que las pedidas, 
+no devolveremos nada, pero debemos indicárselo al usuario.
+
+• cartasMonton(): mostramos aquellas cartas que ya han salido, si no ha 
+salido ninguna indicárselo al usuario
+
+• mostrarBaraja(): muestra todas las cartas hasta el final. Es decir, 
+mostrará las cartas que no se han sacado.
+ */
+    private int numero;
+    private String palo;
+    
+    
+    public void barajar(){
+    
+    }
+    
+    public void siguienteCarta(){
+    
+    }
+    
+    public void cartasDisponibles(){
+        
+    }
+    
+    public void darCartas(){
+        
+    }
+    
+    public void cartasMonton(){
+        
+    }
+    
+    public void mostrarBaraja(){
+        
+    }
+
+    @Override
+    public String toString() {
+        return "Cartas{" + "numero=" + numero + ", palo=" + palo + '}';
+    }
+    
+    
+    
+    
+    
+}
